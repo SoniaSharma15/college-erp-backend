28a34fd12870fad4ba6e735f8b40bc9c85855257
hariom-singh0708 <hs285065@gmail.com>
2026-04-18 12:24:35 +0530

    Course Branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,17 @@ import { Router } from "express";
 
 import authRoutes from "../modules/auth/auth.routes.js";
 import collegeRoutes from "../modules/college/college.routes.js";
+import courseRoutes from "../modules/course/course.routes.js";
+import branchRoutes from "../modules/branch/branch.routes.js";
+
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/college", collegeRoutes);
 
+router.use("/api/courses", courseRoutes);
+router.use("/api/branches", branchRoutes);
 
 
 // test route
